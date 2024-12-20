de6335668dd5a1067ad588a76cc10f92ec5a1d38
Gaia Azizyan <gaia@peach.dev>
2024-12-20 13:49:07 +0100

    Adds action example

--- actions/marketing_emails/manifest.py
+++ actions/marketing_emails/manifest.py
@@ -1,14 +1,8 @@
 import twirl
 
 twirl.manifest(
-    twirl.Table(
-        schema=twirl.Schema(
-            [
-                twirl.Column("date", twirl.TimestampWithoutZone(unit="us")),
-                twirl.Column("sends", twirl.Integer()),
-            ]
-        ),
-        job=twirl.PythonJob(twirl.UpdateMethod.APPEND),
+    twirl.Action(
+        job=twirl.PythonJob(twirl.UpdateMethod.NONE),
         trigger_conditions=twirl.TriggerAt(cron_string="0 0 * * MON-FRI"),
         inputs=[
             twirl.Input("dim_customers"),
